ea61436042c554a7e395cce580a9505d1ffb0c7b
RubixUnderlord <57545547+RubixUnderlord@users.noreply.github.com>
2020-01-10 17:43:28 -0700

    question now continue with timer

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,55 +1,75 @@
-$(document).ready(function(){
+$(document).ready(function () {
     questions = [{
         question: "What is 1 + 1?",
-        answers:["1","2","3","4"],
-        correctAnswer: "2"
+        answers: ["1", "2", "3", "4"],
+        correctAnswer: "2",
+    }, {
+        question: "what is 2 + 2"
     }];
+    var questionsAnswered = 0;
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
     var timer = 30;
 
 
-    $("#start").on("click", function(){
-        start() 
+    $("#start").on("click", function () {
+        start()
     });
-    $("#check").on("click",function(){
+    $("#check").on("click", function () {
         checkifCorrect()
     });
 
-    function start(){
+
+    function game() {
         $(".game").empty();
         countdown()
+
+        var problem = $("<h2>");
+        problem.text(questions[questionsAnswered].question);
+        $(".game").append(problem);
+        for (i = 0; i < questions[0].answers.length; i++) {
+            var solutions = $("<button>");
+            solutions.text(questions[0].answers[i]).attr("id", "check").attr("data-value", questions[0].answers[i]);
+            $(".game").append(solutions);
+        }
+        questionsAnswered++
+    }
+
+    function start() {
         game()
     }
-    function countdown(){
+    function countdown() {
+        timer = 30
         var time = $("<p>");
         time.addClass("timer").text("Time Remaining: " + timer);
         $(".game").append(time);
-        var questionTimer = setInterval(function(){
+        var questionTimer = setInterval(function () {
             timer--;
             $(".timer").text("Time Remaining: " + timer);
             if (timer <= 0) clearInterval(questionTimer);
-        },1000);
+        }, 1000);
+        setTimeout(function () {
+            game()
+        }, 36000);
+
     }
-    function game(){
-        var problem = $("<h2>");
-        problem.text(questions[0].question);
-        $(".game").append(problem);
-        for (i = 0 ; i < questions[0].answers.length;i++) {
-            var solutions = $("<button>");
-            solutions.text(questions[0].answers[i]).attr("id","check").attr("data-value",questions[0].answers[i]);
-            $(".game").append(solutions);
-        }
-        
+    while (timer === 0) {
+        game()
     }
-    function checkifCorrect(){
-        if($(this) === questions[0].correctAnswer){
-            console.log("you got it");
-        }else{
-            console.log("damn");
-        }
-    }
+    //function game() {
+    //
+
+
+
+    // }
+    // function checkifCorrect() {
+    //   if ($(this) === questions[0].correctAnswer) {
+    //        console.log("you got it");
+    //    } else {
+    //        console.log("damn");
+    //    }
+    // }
 });
 
 
